c51cbfedd917e958ccbf8e02fe52c5648a5ae771
Grenard Madrigal <grenardmadrigal@gmail.com>
2019-04-06 16:38:08 -0700

    updated layout and css classes for lead form

--- layout/lead-form.php
+++ layout/lead-form.php
@@ -7,22 +7,24 @@
 
 <form class="form_listing-lead" action="<?=esc_url($_SERVER['REQUEST_URI'])?>" method="post">
 
-<?
+    <?
     if ($_GET['rlid']) {
         $listing = $spark->api->getListingByPostName($_GET['rlid']);
-?>
-    <input type="hidden" name="listing_id" value="<?=$_GET['rlid']?>" />
-    <h1>I'm interested in <?=$listing['address']?> in <?=$listing['city']?></h1>
+        ?>
+        <input type="hidden" name="listing_id" value="<?=$_GET['rlid']?>" />
+        <h1>I'm interested in <?=$listing['address']?> in <?=$listing['city']?></h1>
 
-<? } else { ?>
+    <? } else { ?>
 
-<? } ?>
+    <? } ?>
 
-<input type="text" class="placeholderNoLabel" name="contact_email" value="<?=$_POST['contact_email']?>" placeholder="Email*" />
-<input type="text" class="placeholderNoLabel" name="contact_name" value="<?=$_POST['contact_name']?>" placeholder="Name" />
+    <input type="text" class="placeholderNoLabel" name="contact_email" value="<?=$_POST['contact_email']?>" placeholder="Email*" />
+    <input type="text" class="placeholderNoLabel" name="contact_name" value="<?=$_POST['contact_name']?>" placeholder="Name" />
 
-<textarea class="placeholderNoLabel"></textarea>
+    <textarea class="textarea_message placeholderNoLabel" placeholder="Message (optional)"></textarea>
 
-<button type="submit" value="submit">Send Me Info</button>
+    <div>
+        <button type="submit" value="submit" class="button_send wpcf7-form-control wpcf7-submit">Send Me Info</button>
+    </div>
 
 </form>
